4c16f71d78a3d2488fcb19791f4095b36e2f53cb
felipesemedo05 <felipe.semedo@eletromidia.com.br>
2024-10-30 14:45:06 -0300

    teste

--- app.py
+++ app.py
@@ -37,22 +37,22 @@ st.sidebar.header("Configurações de Credenciais")
 # auth_provider_x509_cert_url = st.sidebar.text_input("URL do Certificado do Provedor de Autenticação")
 # client_x509_cert_url = st.sidebar.text_input("URL do Certificado do Cliente")
 # universe_domain = st.sidebar.text_input("Domínio do Universo")
-# url_api_ooh = st.sidebar.text_input("URL da API OOH")
-# url_api_OnD = st.sidebar.text_input("URL da API OnD")
+url_api_ooh = st.secrets['url_api_ooh']
+url_api_OnD = st.secrets['url_api_OnD']
 
 # Definindo as informações da conta de serviço
 service_account_info = {
-    "type": type,
-    "project_id": project_id,
-    "private_key_id": private_key_id,
-    "private_key": private_key,
-    "client_email": client_email,
-    "client_id": client_id,
-    "auth_uri": auth_uri,
-    "token_uri": token_uri,
-    "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
-    "client_x509_cert_url": client_x509_cert_url,
-    "universe_domain": universe_domain
+    "type": st.secrets['type'],
+    "project_id": st.secrets['project_id'],
+    "private_key_id": st.secrets['private_key_id'],
+    "private_key": st.secrets['private_key'],
+    "client_email": st.secrets['client_email'],
+    "client_id": st.secrets['client_id'],
+    "auth_uri": st.secrets['auth_uri'],
+    "token_uri": st.secrets['token_uri'],
+    "auth_provider_x509_cert_url": st.secrets['auth_provider_x509_cert_url'],
+    "client_x509_cert_url": st.secrets['client_x509_cert_url'],
+    "universe_domain":st.secrets['universe_domain']
 }
 
 # Inputs para a requisição da API
