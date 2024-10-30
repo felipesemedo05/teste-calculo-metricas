cc8e3e7d043d7be77a64815b8582a1a31b80738e
felipesemedo05 <felipe.semedo@eletromidia.com.br>
2024-10-30 14:31:08 -0300

    teste

--- app.py
+++ app.py
@@ -1,87 +1,54 @@
-import streamlit as st
 import pandas as pd
-import pickle
+from datetime import datetime, timedelta
+import google.auth.transport.requests
+import google.oauth2.id_token
+import google.oauth2.service_account
+from tqdm import tqdm
+import json
+import time
+import numpy as np
+import requests
+import certifi
+import urllib3
+import streamlit as st
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Função principal
-def main():
-    # Título do aplicativo
-    st.title("Cálculo Métricas Teste a")
+service_account_info = {
+  "type": type,
+  "project_id": project_id,
+  "private_key_id": private_key_id,
+  "private_key": private_key,
+  "client_email": client_email,
+  "client_id": client_id,
+  "auth_uri": auth_uri,
+  "token_uri": token_uri,
+  "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
+  "client_x509_cert_url": client_x509_cert_url,
+  "universe_domain": universe_domain
+}
 
-    # Leitura do arquivo CSV
-    try:
-        df = pd.read_csv('tabela_metrica_produtos_ruas_bronze.csv')
-    except FileNotFoundError:
-        st.error("Erro: O arquivo 'tabela_metricas_produtos.csv' não foi encontrado.")
-        return
-    
-    try:
-        with open('parametrosPracas.pkl', 'rb') as file:
-            params = pickle.load(file)
-    except FileNotFoundError:
-        st.error("Erro: O arquivo 'parametrosPracas.pkl' não foi encontrado.")
-        return
+url_api_ooh = url_api_ooh
+url_api_OnD = url_api_OnD
 
-    # Verificação se a coluna 'chave_produto' existe no DataFrame
-    if 'chave_produto' not in df.columns:
-        st.error("Erro: A coluna 'chave_produto' não foi encontrada no arquivo.")
-        return
+# Obtenção do ID token
+id_token_ooh = get_id_token(service_account_info, url_api_ooh)
 
-    # Caixa de seleção múltipla com todas as opções de 'chave_produto', filtrável conforme o usuário digita
-    chaves_selecionadas = st.multiselect("Buscar chaves de produto:", df['chave_produto'].unique())
+# Cabeçalho de autenticação
+headers_ooh = {
+    'Authorization': f'Bearer {id_token_ooh}',
+    'Content-Type': 'application/json'
+}
 
-    # Exibir as chaves selecionadas, o período e a cota
-    if chaves_selecionadas:
-        # Filtrar o DataFrame para conter apenas as chaves selecionadas
-        df_filtrado = df[df['chave_produto'].isin(chaves_selecionadas)]
+# Dados a serem enviados na requisição POST
+json_data_ooh = {
+    "map_id": "Mub_Carnaval!",
+    # "map_id": '018f9650-989a-7a89-acfe-6e3c849d6a82',
+    "start_date": "2024-05-07",
+    "end_date": "2024-05-27",
+    "map_name": 1,
+    "force": 0,
+    "csv": 0}
 
-        if not df_filtrado.empty:
-            # Encontrar a linha com o maior valor de 'alcance'
-            maior_alcance = df_filtrado.loc[df_filtrado['alcance'].idxmax()]
+response = requests.post(url_api_ooh, headers=headers_ooh, json=json_data_ooh, verify=False)
 
-            # Exibir os detalhes do produto com o maior alcance
-            st.write(f"Produto com maior alcance: {maior_alcance['chave_produto']}")
-            st.write(f"Alcance: {maior_alcance['alcance']}")
-
-                        # Exibir os detalhes do produto com o maior alcance
-            st.write(f"Produto com maior alcance: {maior_alcance['chave_produto']}")
-            st.write(f"Alcance: {maior_alcance['alcance']}")
-
-            # Exibir os pontos do produto com o maior alcance
-            st.write(f"Pontos do produto com maior alcance: {maior_alcance['pontos']}")
-            
-            # Somar os pontos de todos os produtos selecionados
-            total_pontos = df_filtrado['pontos'].sum()
-
-            # Exibir a soma dos pontos
-            st.write(f"Soma dos pontos dos produtos selecionados: {total_pontos}")
-        else:
-            st.write("Nenhuma chave foi encontrada após a filtragem.")
-    else:
-        st.write("Nenhuma chave selecionada.")
-        
-    # Campo de entrada de texto para o período
-    periodo = st.text_input("Digite o período:")
-
-    # Caixa de seleção para a cota
-    cota = st.selectbox("Selecione a cota:", ['0.5', '1.0', '2.0'])
-
-    # Exibir as chaves selecionadas
-    texto_chaves = ', '.join(chaves_selecionadas)
-    st.write(f"Chaves selecionadas: {texto_chaves}")
-
-    # Exibir o período
-    st.write(f"Período inserido: {periodo}")
-
-    # Exibir a cota selecionada
-    st.write(f"Cota selecionada: {cota}")
-
-    lista_chaves = chaves_selecionadas
-
-    lista_pracas = df[df['chave_produto'].isin(lista_chaves)]['praca'].unique()
-
-    texto_pracas = ', '.join(lista_pracas)
-    st.write(f"Praças presentes: {texto_pracas}")
-
-# Executa o aplicativo
-if __name__ == '__main__':
-    main()
+st.write(response)
